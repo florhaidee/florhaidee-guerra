7ef1a70bfa615164594663a887ff8be7fdda590a
Florhaidee <florhaideedev@gmail.com>
2020-09-21 17:47:52 -0600

    Updates

---
+++ precache-manifest.6679912c057998501015e1525a62e58f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f3872e2dd16c7a79b07b44ff260ff4",
+    "url": "/florhaidee-guerra/index.html"
+  },
+  {
+    "revision": "4134a383cbcc6ff86662",
+    "url": "/florhaidee-guerra/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "3121345b94a4cfc793a3",
+    "url": "/florhaidee-guerra/static/css/main.98ebdce2.chunk.css"
+  },
+  {
+    "revision": "4134a383cbcc6ff86662",
+    "url": "/florhaidee-guerra/static/js/2.e228e604.chunk.js"
+  },
+  {
+    "revision": "6485e28f1bc40c1e743bb4de5f286310",
+    "url": "/florhaidee-guerra/static/js/2.e228e604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3121345b94a4cfc793a3",
+    "url": "/florhaidee-guerra/static/js/main.0208581f.chunk.js"
+  },
+  {
+    "revision": "13f839ec4fb74195aba4",
+    "url": "/florhaidee-guerra/static/js/runtime-main.8b52a040.js"
+  },
+  {
+    "revision": "d836129c59bc7f2098c8ea099c92c59d",
+    "url": "/florhaidee-guerra/static/media/me.d836129c.jpg"
+  }
+]);
